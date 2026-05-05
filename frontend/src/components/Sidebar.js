43f0cbc104060c7f30b1793eee2a5ae11a2f5eae
Vesa <149439049+grajqa@users.noreply.github.com>
2026-05-05 21:14:15 +0200

    updated sidebar components

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -8,10 +8,14 @@ export default function Sidebar() {
 
       <Link className="link" to="/">Dashboard</Link>
       <Link className="link" to="/therapists">Therapists</Link>
+      <Link className="link" to="/patients">Patients</Link>
+      <Link className="link" to="/treatments">Treatments</Link>
+      <Link className="link" to="/treatment-plans">Treatments Plan</Link>
       <Link className="link" to="/sessions">Sessions</Link>
       <Link className="link" to="/exercises">Exercises</Link>
-      <Link className="link" to="/patients">Patients</Link>
+      <Link className="link" to="/exercise-plans">Exercises Plan</Link>
       <Link className="link" to="/equipment">Equipment</Link>
+      <Link className="link" to="/payments">Payments</Link>
       <Link className="link" to="/assessments">Assessments</Link>
     </div>
   );
