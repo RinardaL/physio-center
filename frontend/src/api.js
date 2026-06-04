bd393d58d82820b5d158006a5fa325998922d61f
RinardaL <rl64883@ubt-uni.net>
2026-06-05 00:35:36 +0200

    fixed interceptor loop

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,16 +5,18 @@ const api = axios.create({
   withCredentials: true,
 });
 
+// REQUEST INTERCEPTOR
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
-  if (token) {
+  if (token && typeof token === "string") {
     config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
 });
 
+// RESPONSE INTERCEPTOR
 api.interceptors.response.use(
   (res) => res,
   async (err) => {
@@ -23,13 +25,24 @@ api.interceptors.response.use(
     if (err.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
-      const res = await api.post("/auth/refresh");
+      try {
+        const res = await axios.post(
+          "http://localhost:3000/api/auth/refresh",
+          {},
+          { withCredentials: true }
+        );
 
-      localStorage.setItem("token", res.data.accessToken);
+        localStorage.setItem("token", res.data.accessToken);
 
-      originalRequest.headers.Authorization = `Bearer ${res.data.accessToken}`;
+        originalRequest.headers.Authorization = `Bearer ${res.data.accessToken}`;
 
-      return api(originalRequest);
+        return api(originalRequest);
+      } catch (refreshError) {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        window.location.href = "/login";
+        return Promise.reject(refreshError);
+      }
     }
 
     return Promise.reject(err);
